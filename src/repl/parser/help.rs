c09bf3920eb1e90bfe2d49e9fdf7224fe25947db
Artur Gulik <artur.gulik@protonmail.com>
2022-07-03 02:01:08 +0200

    Modify print_help function to make command documentation possible in the future

--- src/repl/parser/help.rs
+++ src/repl/parser/help.rs
@@ -1,19 +1,31 @@
 use colored::*;
 
-pub fn print_help() {
-    println!(
-"
-The list of available commands:
+pub fn print_help(arg: &str) {
+    match arg.to_lowercase().as_str() {
+    "" => {
+        println!(
+        "
+        The list of available commands:
 
-{}, {} -- display events or calendars
-{}, {} -- create events or calendars
-{}, {} -- remove specified events or calendars
+        {}, {} -- display events or calendars
+        {}, {} -- create events or calendars
+        {}, {} -- remove specified events or calendars
 
-Type \"help\" followed by command name for full documentation.
-",
-
-"show".bold(), "sho, sh, s".dimmed(),
-"new".bold(), "ne, n".dimmed(),
-"remove".bold(), "rem, re, r".dimmed());
+        Type \"help\" followed by command name for full documentation.
+        ",
+        "show".bold(), "sho, sh, s".dimmed(),
+        "new".bold(), "ne, n".dimmed(),
+        "remove".bold(), "rem, re, r".dimmed());
+    },
+    "show"|"sho"|"sh"|"s" => {
+        println!(
+	"
+	{}
+	",
+        "show, sho, sh, s".bold());
+    }
+    _ => {
+        println!("No documentation entries for: {}", arg);
+    }
+    }
 }
-
