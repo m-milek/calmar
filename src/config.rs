d1964fab9323cf2000b67c42355c0e0abbf2e232
Michał Miłek <michal.mileq@gmail.com>
2022-08-06 16:44:18 +0200

    Add prompt customization

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,11 @@ pub struct Config {
     pub date_format: String,
     pub time_format: String,
     pub default_path: String,
+    pub prompt_text: String,
+    pub prompt_color: String,
+    pub prompt_bold: bool,
+    pub prompt_italic: bool,
+    pub prompt_underline: bool
     //TODO: add fields
 }
 
@@ -10,6 +15,23 @@ pub fn get_config() -> Config {
         date_format: "DD/MM/YYYY".to_string(),
         time_format: "HH:MM".to_string(),
         default_path: "/home/michal/.calmar".to_string(),
+	prompt_text: "[calmar]".to_string(),
+	/*
+	Available colors:
+	- black
+	- red
+	- green
+	- yellow
+	- blue
+	- magenta
+	- cyan
+	- white
+	- bright_*
+	*/
+	prompt_color: "white".to_string(),
+	prompt_bold: true,
+	prompt_italic: false,
+	prompt_underline: false,
     }
     //TODO: find a config file in .config/calmar
     //TODO: config.json
--- src/repl.rs
+++ src/repl.rs
@@ -1,5 +1,7 @@
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 use std::io::{self, Write};
+
+use crate::CONFIG;
 pub mod parser;
 
 /*
@@ -19,13 +21,54 @@ fn get_input() -> String {
     input.as_str().trim().to_owned()
 }
 
+fn print_prompt() {
+    let prompt_text = &CONFIG.prompt_text;
+    let mut prompt = prompt_text.white();
+    
+    if CONFIG.prompt_bold {
+	prompt = prompt.bold();
+    }
+
+    if CONFIG.prompt_italic {
+	prompt = prompt.italic();
+    }
+
+    if CONFIG.prompt_underline {
+	prompt = prompt.underline();
+    }
+
+    // Colorize the prompt
+    prompt = match CONFIG.prompt_color.as_str() {
+	"black" => prompt.black(),
+	"bright_black" => prompt.bright_black(),
+	"red" => prompt.red(),
+	"bright_red" => prompt.bright_red(),
+	"green" => prompt.green(),
+	"bright_green" => prompt.bright_green(),
+	"yellow" => prompt.yellow(),
+	"bright_yellow" => prompt.bright_yellow(),
+	"blue" => prompt.blue(),
+	"bright_blue" => prompt.bright_blue(),
+	"magenta" => prompt.magenta(),
+	"bright_magenta" => prompt.bright_magenta(),
+	"cyan" => prompt.cyan(),
+	"bright_cyan" => prompt.bright_cyan(),
+	"white" => prompt,
+	"bright_white" => prompt.bright_white(),
+ 	_ => "INVALID CONFIG".red().bold()
+    };
+
+    print!("{} ", prompt);
+}
+
 /*
 Continously get input and handle it until the process ends
 */
 pub fn run() {
     let mut input;
     loop {
-        print!("{}", "[calmar] ".bold());
+        //print!("{}", "[calmar] ".bold());
+	print_prompt();
         input = get_input();
         match input.as_str() {
             "" => (),
